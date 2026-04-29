798586e3624f2ea0ba871bb3411b98abc8b71037
Aqsa <aqsaabdullah38403@gmail.com>
2026-04-29 21:45:44 +0500

    add layout

--- app/page.js
+++ app/page.js
@@ -1,7 +1,37 @@
-import React from 'react';
+"use client";
+import React, { useEffect } from 'react';
 import ScreenManager from '../component/ScreenManager.jsx';
+import "./globals.css"
 
 function App() {
+
+  useEffect(() => {
+    // Disable right click
+    const handleContextMenu = (e) => {
+      e.preventDefault();
+    };
+
+    // Disable text selection
+    const handleSelectStart = (e) => {
+      e.preventDefault();
+    };
+
+    // Disable drag (images, links, etc.)
+    const handleDragStart = (e) => {
+      e.preventDefault();
+    };
+
+    document.addEventListener('contextmenu', handleContextMenu);
+    document.addEventListener('selectstart', handleSelectStart);
+    document.addEventListener('dragstart', handleDragStart);
+
+    return () => {
+      document.removeEventListener('contextmenu', handleContextMenu);
+      document.removeEventListener('selectstart', handleSelectStart);
+      document.removeEventListener('dragstart', handleDragStart);
+    };
+  }, []);
+
   return (
     <div className="App">
       <ScreenManager />
